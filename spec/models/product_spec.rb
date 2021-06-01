d6e14ecdcd4a2ab1d87b77c9ba027de8379eda7f
Siddhesh Acharekar <siddhesh.acharekar@mindbodyonline.com>
2021-06-01 11:37:48 -0500

    removed rubocop offenses

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,28 +1,30 @@
-require "rails_helper"
+# frozen_string_literal: true
 
-RSpec.describe Product, :type => :model do
-  #TODO: clean up this test
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  # TODO: clean up this test
 
   subject { described_class.new }
 
-  fit "is valid with valid attributes" do
-    subject.title = "Test"
-    subject.description = "Test"
-    subject.image_url = "Test.jpg"
-    subject.price = "26"
+  it 'is valid with valid attributes' do
+    subject.title = 'Test'
+    subject.description = 'Test'
+    subject.image_url = 'Test.jpg'
+    subject.price = '26'
 
     expect(subject).to be_valid
   end
 
-  it "is not valid without a title" do
+  it 'is not valid without a title' do
     subject.title = nil
-    subject.description = "Test"
-    subject.image_url = "Test.jpg"
-    subject.price = "26"
+    subject.description = 'Test'
+    subject.image_url = 'Test.jpg'
+    subject.price = '26'
     expect(subject).to_not be_valid
   end
 
-  it "is not valid without a description"
-  it "is not valid without a image_url"
-  it "is not valid without a price"
+  it 'is not valid without a description'
+  it 'is not valid without a image_url'
+  it 'is not valid without a price'
 end
